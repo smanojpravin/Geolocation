5c8b4b117b4520eca2f978a11c1a77d8ef9f19b4
Manoj Pravin <smanojpravin@gmail.com>
2020-12-22 22:40:05 +0530

    8th commit

--- goelocation/settings.py
+++ goelocation/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
